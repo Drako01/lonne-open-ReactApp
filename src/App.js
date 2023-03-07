359c5c29ef8dce0e042f62b5800db996302c0c86
Alejandro Di Stefano <addistefano@live.com.ar>
2023-03-07 11:47:57 -0300

    Actualizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './components/assets/img/logo.png';
+import Navbar from './components/js/Navbar.js'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />  
         <h2>PRO SHOP</h2>   
---
+++ src/components/js/Navbar.js
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="Navbar">
+                <h1>Bienvenidos</h1>
+            </div>            
+            <div className="NavbarButtons">
+                <button>Raquetas</button>
+                <button>Pelotas</button>
+                <button>Vinchas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
