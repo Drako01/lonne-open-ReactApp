ad73575841b8796afbe4b33b2fdf6f5e023e5cb8
Alejandro Di Stefano <addistefano@live.com.ar>
2023-03-13 20:36:36 -0300

    1er Entrega

--- src/components/js/Main.js
+++ src/components/js/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
     return (
         <main className="App-main">
             <ItemListContainer greeting={'Bienvenidos a mi E-Commerce'}/>
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={ logo } className="App-logo" alt="logo" />
             <h2>PRO SHOP</h2>
             <h3>Pronto conocerás nuestras novedades</h3>    
             <WhatsAppButton />       
