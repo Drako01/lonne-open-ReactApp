c3c8e91cad7dc4d76566095471220559e4b2be48
Alejandro Di Stefano <addistefano@live.com.ar>
2023-03-21 19:55:11 -0300

    2ds Entrega

--- src/components/js/Main.js
+++ src/components/js/Main.js
@@ -1,13 +1,11 @@
 import ItemListContainer from './ItemListContainer'
 import CardsPanel from './CardsPanel';
-import CarrouselPhotos from './Carrousel';
 
 const Main = () => {
     return (
         <main className="App-main">
             <ItemListContainer greeting={'PRO SHOP'}/>
             <CardsPanel />
-            <CarrouselPhotos />
         </main>
     )
 }
