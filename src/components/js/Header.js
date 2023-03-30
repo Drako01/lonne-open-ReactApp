1ecfea2fae4865c572e7c8d35b5509e43ab33c6f
Alejandro Di Stefano <addistefano@live.com.ar>
2023-03-30 19:47:52 -0300

    Carrito

--- src/components/js/Header.js
+++ src/components/js/Header.js
@@ -1,13 +1,9 @@
 import Navbar from './Navbar'
-import { CartProvider } from '../../context/CartContext';
 
 const Header = () => {
     return (
-        <header className="App-header">
-            <CartProvider>
-                <Navbar />
-            </CartProvider>
-
+        <header className="App-header">           
+            <Navbar />    
         </header>
     )
 }
