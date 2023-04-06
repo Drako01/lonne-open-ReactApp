a5bb7519ec1172de39d9ac33bb7c7d82132ae8e6
Alejandro Di Stefano <addistefano@live.com.ar>
2023-04-06 11:27:32 -0300

    Footer

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -2,6 +2,8 @@ import whatsapp from '../assets/icons/whatsapp.svg';
 import instagram from '../assets/icons/instagram.svg';
 import facebook from '../assets/icons/facebook.svg';
 import WhatsAppButton from './WhatsAppButton';
+import logo from '../assets/icons/logo.ico'
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
     const mensajeWhatsApp = () => {
@@ -13,18 +15,45 @@ const Footer = () => {
     const mensajeFacebook = () => {
         window.open("https://www.facebook.com/people/Lonn%C3%A9-Open-Tenis/100063638766614/?locale=es_LA")
     }
-    
+
     return (
         <div className="Footer">
-            <section className='Iconos-social'>
-                <img src={whatsapp} alt="icono" onClick={mensajeWhatsApp}/>
-                <img src={instagram} alt="icono" onClick={mensajeInstagram} />
-                <img src={facebook} alt="icono" onClick={mensajeFacebook} />
-            </section>
-            <WhatsAppButton /> 
-            <p>
-                &copy; Alejandro Di Stefano | Entrega Final | Curso de React en CoderHouse | Comisión #39610
-            </p>             
+            <footer className='Top'>
+                <img src={logo} alt='Lonne Open' />
+                <section className='Links'>
+                    <div>
+                    <NavLink to='/'><li>Inicio</li></NavLink>
+                    <NavLink to='/category/Raquetas' ><li>Raquetas</li></NavLink>
+                    <NavLink to='/category/Tubos' ><li>Tubos</li></NavLink>
+                    <NavLink to='/category/Zapatillas'><li>Zapatillas</li></NavLink>
+                    </div>
+                    <div>
+                    <NavLink to='/category/Remeras' ><li>Remeras</li></NavLink>
+                    <NavLink to='/category/Munequeras' ><li>Muñequeras</li></NavLink>
+                    <NavLink to='/category/Vinchas' ><li>Vinchas</li></NavLink>
+                    <NavLink to='/contact' ><li>Contactenos</li></NavLink>
+                    </div>
+                    
+                    
+                </section>
+            </footer>
+            <footer className='Bottom'>
+                <div className='Legal'>
+                    <p>
+                        &copy; Alejandro Di Stefano | Entrega Final | Curso de React en CoderHouse | Comisión #39610
+                    </p>
+                </div>
+                <section className='Iconos-social Links'>
+                    <img src={whatsapp} alt="icono" onClick={mensajeWhatsApp} />
+                    <img src={instagram} alt="icono" onClick={mensajeInstagram} />
+                    <img src={facebook} alt="icono" onClick={mensajeFacebook} />
+                </section>
+
+
+
+            </footer>
+
+            <WhatsAppButton />
         </div>
     )
 }
