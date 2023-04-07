6d46752ced4f681d2a30cb544a004496da3a4ffd
Alejandro Di Stefano <addistefano@live.com.ar>
2023-04-07 20:03:27 -0300

    List Detail

--- src/components/js/Checkout.js
+++ src/components/js/Checkout.js
@@ -1,16 +1,77 @@
 import CreditCardForm from './CreditCard';
+import { useCart } from '../../context/CartContext';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Checkout = () => {
-    return (
-        <div className='Checkout'>
-            <h1 className='Mini'>
-                Payment
-            </h1>
-            <section className='Checkout__'>
-                <CreditCardForm />
-            </section>            
-        </div>
-    )
-}
+    const { cart, totalPrice, clearCart, setPurchaseHistory } = useCart();
+    const navigate = useNavigate();
 
-export default Checkout
+    const vaciarCarrito = () => {
+        clearCart();
+    };
+
+    const handlePayment = () => {
+        Swal.fire({
+            title: '¡Compra exitosa!',
+            text: 'Gracias por comprar en Lonne Open',
+            icon: 'success',
+            didClose: () => {
+                vaciarCarrito();
+                navigate('/');
+            }
+        });
+    };
+    
+    const handleOnClick = () => {
+        const purchase = {
+            date: new Date(),
+            total: totalPrice,
+            products: cart,
+        };
+        setPurchaseHistory((prevHistory) => [...prevHistory, purchase]);
+        handlePayment();
+    };
+
+    return (
+        <div>
+            <h1 className="Mini">Checkout</h1>
+            <div>
+                <h3>Resumen de Compra</h3>
+                <table className="ItemListDetail">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart.map((product) => (
+                            <tr key={product.id}>
+                                <td className='LeftItem'>{product.name}</td>
+                                <td>${product.price}</td>
+                                <td>{product.quantity}</td>
+                                <td>${product.price * product.quantity}</td>
+                            </tr>
+                        ))}
+                        <tr className="total-row">
+                            <td className='PriceProducto' colSpan="3 ">Total:</td>
+                            <td className='PriceProducto'>${totalPrice}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="checkout-payment">
+                <h3>Payment Details</h3>
+                <CreditCardForm handlePayment={handlePayment} />
+                <div className="ComprarFinal FinalButtons">
+                    <button onClick={handleOnClick}>Confirmar</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
--- src/components/js/ItemListDetail.js
+++ src/components/js/ItemListDetail.js
@@ -1,77 +1,96 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import { getDoc, doc, collection, getDocs } from 'firebase/firestore'
-import { db } from '../../Firebase/firebaseConfig'
-
+import { getDoc, doc, collection, getDocs } from 'firebase/firestore';
+import { db } from '../../Firebase/firebaseConfig';
+import { useCart } from '../../context/CartContext'
+import carrito from '../assets/icons/carro.png';
 
 const ItemListDetail = () => {
-
     const navigate = useNavigate();
     const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
     const { itemId } = useParams();
+    const { addItem } = useCart();
+    const [quantity, setQuantity] = useState(1);
 
-    useEffect(() => {
-        setLoading(true)
-
-        if (itemId) {
-            const productRef = doc(db, 'products', itemId)
-
-            getDoc(productRef)
-                .then(snapshot => {
-                    const data = snapshot.data()
-                    const productAdapted = { id: snapshot.id, ...data }
-                    setProducts([productAdapted])
-                })
-                .catch(error => {
-                    Swal.fire("Error", error.message, "error");
-                })
-                .finally(() => {
-                    setLoading(false)
-                })
-        } else {
-            const productsRef = collection(db, 'products')
-
-            getDocs(productsRef)
-                .then(snapshot => {
-                    const productsData = snapshot.docs.map(doc => {
-                        const data = doc.data()
-                        const productAdapted = { id: doc.id, ...data }
-                        return productAdapted
-                    }).sort((a, b) => a.category.localeCompare(b.category))
-                    setProducts(productsData)
-                })
-                .catch(error => {
-                    Swal.fire("Error", error.message, "error");
-                })
-                .finally(() => {
-                    setLoading(false)
-                })
-        }
-    }, [itemId])
 
     const handleOnClick = () => {
         navigate('/');
     };
 
+    const handleOnAddToCart = (product) => {
+        addItem({ ...product, quantity });
+        setQuantity(product.quantity);
+        Swal.fire(`El Producto ${product.name} fue agregado al carrito`, '', 'success');
+    };
+
+    useEffect(() => {
+        setLoading(true);
+
+        if (itemId) {
+            const productRef = doc(db, 'products', itemId);
+
+            getDoc(productRef)
+                .then((snapshot) => {
+                    const data = snapshot.data();
+                    const productAdapted = { id: snapshot.id, ...data };
+                    productAdapted.quantity = 1;
+                    setProducts([productAdapted]);
+                })
+                .catch((error) => {
+                    Swal.fire('Error', error.message, 'error');
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        } else {
+            const productsRef = collection(db, 'products');
+
+            getDocs(productsRef)
+                .then((snapshot) => {
+                    const productsData = snapshot.docs.map((doc) => {
+                        const data = doc.data();
+                        const productAdapted = { id: doc.id, ...data };
+                        productAdapted.quantity = 1;
+                        return productAdapted;
+                    }).sort((a, b) => a.category.localeCompare(b.category));
+                    setProducts(productsData);
+                })
+                .catch((error) => {
+                    Swal.fire('Error', error.message, 'error');
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }
+    }, [itemId]);
+
+    const handleOnQuantityChange = (e, productId) => {
+        const value = parseInt(e.target.value) || 1;
+        setProducts(prevState => prevState.map(product => {
+            if (product.id === productId) {
+                return { ...product, quantity: value };
+            } else {
+                return product;
+            }
+        }));
+    };
+
     return (
         <div>
-
-            {loading ?
+            {loading ? (
                 <div className="loader-container">
                     <div className="loader"></div>
                     <div className="loader2"></div>
                 </div>
-                :
+            ) : (
                 <section>
-                    <div className='ButtonItemListDetail'>
-                        <button onClick={handleOnClick} >
-                            Volver
-                        </button>
+                    <div className="ButtonItemListDetail">
+                        <button onClick={handleOnClick}>Volver</button>
                     </div>
 
-                    <table className='ItemListDetail'>
+                    <table className="ItemListDetail">
                         <thead>
                             <tr>
                                 <th>Nombre</th>
@@ -81,26 +100,49 @@ const ItemListDetail = () => {
                                 <th>Size</th>
                                 <th>Foto</th>
                                 <th>Stock</th>
+                                <th>Cantidad</th>
+                                <th>Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {products.map(product => (
-                                <tr key={product.id}>
-                                    <td className='LeftItem'>{product.name}</td>
-                                    <td className='LeftItem'>{product.category}</td>
-                                    <td className='LeftItem'>{product.description}</td>
-                                    <td className='RightItem'>${product.price}.-</td>
-                                    <td>{product.size}</td>
-                                    <td><img src={product.image} alt={product.name} /></td>
-                                    <td>{product.stock}</td>
+                            {Object.values(products).map((producto) => (
+                                <tr key={producto.id}>
+                                    <td className='LeftItem'>{producto.name}</td>
+                                    <td>{producto.category}</td>
+                                    <td>{producto.description}</td>
+                                    <td>${producto.price}</td>
+                                    <td>{producto.size}</td>
+                                    <td>
+                                        <img src={producto.image} alt={producto.title} />
+                                    </td>
+                                    <td>{producto.stock}</td>
+                                    <td>
+                                        <input
+                                            type="number"
+                                            min="1"
+                                            max={producto.stock}
+                                            onChange={(e) => handleOnQuantityChange(e, producto.id)}
+                                            value={producto.quantity}
+
+                                        />
+                                    </td>
+                                    <td>
+                                        <div className='ComprarFinal FinalButtons CarritoListButton'>
+                                            <button
+                                                type="submit"
+                                                onClick={() => handleOnAddToCart({ ...producto, quantity })}
+                                            >
+                                                <img src={carrito} className="App-icono Car CarritoList" alt="icono" />
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
                             ))}
-                        </tbody>
 
+                        </tbody>
                     </table>
                 </section>
-            }
-
+            )}
         </div>
     );
 };
