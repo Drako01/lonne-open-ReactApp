b286c231c90466d21915530e8f7993c8b25ae2ec
Alejandro Di Stefano <addistefano@live.com.ar>
2023-04-07 12:27:00 -0300

    Firebase

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -16,4 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
-export const auth = getAuth();
