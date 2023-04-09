9fa986444d07b91900aadd37b4aa64434ba21e03
Alejandro Di Stefano <addistefano@live.com.ar>
2023-04-08 23:07:41 -0300

    Carrito Oculto en Navegacion

--- src/components/js/CartWidget.js
+++ src/components/js/CartWidget.js
@@ -1,17 +1,25 @@
 import carrito from '../assets/icons/carro.png';
 import { useCart } from '../../context/CartContext';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const CartWidget = () => {
-    
-    const { totalQuantity } = useCart()
 
+    const { totalQuantity } = useCart()
+    const location = useLocation();
+    
     return (
         <li>
-            <Link to={'/cart'} className="delay09 CartWidget">
-                <img src={carrito} className="App-icono Cart" alt="icono" /> <h4>{totalQuantity}</h4>
-            </Link>
-            
+            {
+                (location.pathname === '/history' || location.pathname === '/contact') ?
+                    <Link to={'/cart'} className="delay09 CartWidget None">
+                        <img src={carrito} className="App-icono Cart" alt="icono" /> <h4>{totalQuantity}</h4>
+                    </Link>
+                    :
+
+                    <Link to={'/cart'} className="delay09 CartWidget">
+                        <img src={carrito} className="App-icono Cart" alt="icono" /> <h4>{totalQuantity}</h4>
+                    </Link>
+            }
         </li>
     )
 }
--- src/components/js/Navbar.js
+++ src/components/js/Navbar.js
@@ -1,14 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import CartWidget from './CartWidget'
 
-
 const Navbar = () => {
     const toggleMenu = () => {
         document.body.classList.toggle('open')
     }
 
     return (
-
         <section>
             <button className='burguer' onClick={toggleMenu}></button>
             <div className='menu'>
@@ -19,10 +17,10 @@ const Navbar = () => {
                     <NavLink to='/category/Zapatillas' className={'delay03'}><li>Zapatillas</li></NavLink>
                     <NavLink to='/category/Remeras' className={'delay04'}><li>Remeras</li></NavLink>
                     <NavLink to='/category/Munequeras' className={'delay05'}><li>Muñequeras</li></NavLink>
-                    <NavLink to='/category/Vinchas' className={'delay06'}><li>Vinchas</li></NavLink>   
-                    <NavLink to='/contact' className={'delay07'}><li>Contactenos</li></NavLink>    
-                    <NavLink to='/history' className={'delay08'}><li>Historial de Compras</li></NavLink>                
-                </nav>                
+                    <NavLink to='/category/Vinchas' className={'delay06'}><li>Vinchas</li></NavLink>
+                    <NavLink to='/contact' className={'delay07'}><li>Contactenos</li></NavLink>
+                    <NavLink to='/history' className={'delay08'}><li>Historial de Compras</li></NavLink>
+                </nav>
             </div>
             <CartWidget />
         </section>
