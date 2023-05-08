d8caf28f637edd385b21e6fc5d96255c859af1a8
Alejandro Di Stefano <addistefano@live.com.ar>
2023-05-08 15:14:10 -0300

    Cards con Material UI

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -1,55 +1,92 @@
 import { Link } from 'react-router-dom';
-import ItemCount from './ItemCount'
-import { useCart } from '../../context/CartContext'
+import { Card, CardContent, CardMedia, Typography, Button, makeStyles } from '@material-ui/core';
+import ItemCount from './ItemCount';
+import { useCart } from '../../context/CartContext';
 
-
+const useStyles = makeStyles(() => ({
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '600px',
+        width: '380px',
+    },
+    media: {
+        height: '280px',
+        width: '100%',
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        flex: 1,
+    },
+    buttonContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: 'auto',
+    },
+}));
 
 const Cards = ({ id, name, price, category, description, size, image, stock }) => {
-
-    const { addItem, getItemCount, isInCart } = useCart()
+    const { addItem, getItemCount, isInCart } = useCart();
+    const classes = useStyles();
 
     const handleOnAdd = (quantity) => {
         const productToAdd = {
-            id, name, price, category, description, size, image, stock, quantity
-        }
-        addItem(productToAdd)
-    }
+            id,
+            name,
+            price,
+            category,
+            description,
+            size,
+            image,
+            stock,
+            quantity,
+        };
+        addItem(productToAdd);
+    };
 
     return (
-        <div className="Card">
-            <img src={image} alt={name} />
-
-            <div className='Dates'>
-                <h2 className='TextDates'>
-                    {name}
-                </h2>
-                <h3 className='TextDates'>
-                    Talle: {size}
-                </h3>
-                <h3 className='Price'>
-                    Precio: $ {price}.-
-                </h3>
-
-                {
-                    isInCart(id) ? (
-                        <Link to={`/${name}/item/${id}`} className="DontView">Detalles</Link>
-                    ) : (
-                        <Link to={`/${name}/item/${id}`}>Detalles</Link>
-                    )
-                }
-                {
-                    isInCart(id) ? (
-                        <Link className='ProductInCart'>
-                            {getItemCount(id) === 1 ? `${getItemCount(id)} Producto en el Carrito` : `${getItemCount(id)} Productos en el Carrito`}
+        <Card className={classes.card}>
+            <CardMedia component="img" alt={name} className={classes.media} image={image} />
+            <CardContent className={classes.content}>
+                <div>
+                    <Typography variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        Talle: {size}
+                    </Typography>
+                    <Typography variant="h5" color="textSecondary">
+                        Precio: $ {price}.-
+                    </Typography>
+                    
+                </div>
+                <div className={classes.buttonContainer}>
+                    {isInCart(id) ? (
+                        <Link to={`/${name}/item/${id}`} className="DontView">
+                            <Button color="secondary">Detalles</Button>
                         </Link>
-                        
+                    ) : (
+                        <Link to={`/${name}/item/${id}`}>
+                            <Button color="secondary">Detalles</Button>
+                        </Link>
+                    )}
+
+                    {isInCart(id) ? (
+                        <Typography variant="body2" className="ProductInCart">
+                            {getItemCount(id) === 1
+                                ? `${getItemCount(id)} Producto en el Carrito`
+                                : `${getItemCount(id)} Productos en el Carrito`}
+                        </Typography>
                     ) : (
                         <ItemCount onAdd={handleOnAdd} stock={stock} />
-                    )
-                }
-            </div >
-        </div >
-    )
-}
+                    )}
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
 
-export default Cards
+export default Cards;
--- src/components/js/ItemDetail.js
+++ src/components/js/ItemDetail.js
@@ -1,59 +1,100 @@
 import { Link } from 'react-router-dom';
-import ItemCount from './ItemCount'
-import { useCart } from '../../context/CartContext'
+import { makeStyles, Typography } from '@material-ui/core';
+import ItemCount from './ItemCount';
+import { useCart } from '../../context/CartContext';
 
-const ItemDetail = ({ id, name, category, description, price, size, image, stock }) => {   
-    
-    const { addItem, isInCart } = useCart()
+const useStyles = makeStyles((theme) => ({
+    cardDetail: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        padding: theme.spacing(2),
+        backgroundColor: '#fff',
+    },
+    image: {
+        width: '380px',
+        marginBottom: theme.spacing(2),
+    },
+    dates: {
+        marginBottom: theme.spacing(2),
+    },
+    price: {
+        fontWeight: 'bold',
+    },
+    button: {
+        marginTop: theme.spacing(2),
+    },
+    payButton: {
+        padding: theme.spacing(1, 2),
+        backgroundColor: '#333',
+        color: '#fff',
+        borderRadius: '4px',
+        textDecoration: 'none',
+        transition: 'background-color 0.3s ease',
+        '&:hover': {
+            backgroundColor: '#333',
+        },
+    },
+}));
+
+const ItemDetail = ({ id, name, category, description, price, size, image, stock }) => {
+    const { addItem, isInCart } = useCart();
+    const classes = useStyles();
 
     const handleOnAdd = (quantity) => {
         const productToAdd = {
-            id, name, price, category, description, size, image, stock, quantity
-        }
-        addItem(productToAdd)
-    }
-
+            id,
+            name,
+            price,
+            category,
+            description,
+            size,
+            image,
+            stock,
+            quantity,
+        };
+        addItem(productToAdd);
+    };
 
     return (
-        <div className="Card CardDetail" id='Item-Detail'>
-
-            <div>
-                <img src={image} alt={name} />
-                <div className='Dates'>
-
-                    <h2>{name}</h2>
-                    <h3>{category}</h3>
-                    <p>
-                        {description}
-                    </p>
-                    <p>
-                        En Stock: {stock}
-                    </p>
-                    <h3>
-                        Talle: {size}
-                    </h3>
-                    <h3 className='Price'>
-                        Precio: $ {price}.-
-                    </h3>
-                </div>
+        <div className={classes.cardDetail}>
+            <img src={image} alt={name} className={classes.image} />
+            <div className={classes.dates}>
+                <Typography variant="h4" component="h2" style={{ color: '#333' }}>
+                    {name}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary" align="left">
+                    {category}
+                </Typography>
+                <Typography variant="body2" align="left" style={{ color: '#333' }}>
+                    {description}
+                </Typography>
+                <Typography variant="body2" align="left" style={{ color: '#333' }}>
+                    En Stock: {stock}
+                </Typography>
+                <Typography variant="subtitle1" align="left" style={{ color: '#333' }}>
+                    Talle: {size}
+                </Typography>
+                <Typography variant="h6" className={classes.price} align="left" style={{ color: '#333' }}>
+                    Precio: $ {price}.-
+                </Typography>
             </div>
-
-            <div className='Btn'>     
-            {
-                    isInCart(id) ? (
-                        <Link to={'/cart'} className='ProductInCart'>Pagar</Link>
-                    ) : (
-                        <div>                       
-                            <ItemCount onAdd={handleOnAdd} stock={stock} />                                                        
-                        </div>
-                        
-                    )
-                } 
-                
+            <div>
+                {isInCart(id) ? (
+                    <Link to="/cart" className={classes.button}>
+                        <Typography variant="button" color="primary" className={classes.payButton}>
+                            Pagar
+                        </Typography>
+                    </Link>
+                ) : (
+                    <ItemCount onAdd={handleOnAdd} stock={stock} />
+                )}
             </div>
 
         </div>
-    )
-}
+    );
+};
 
-export default ItemDetail
+export default ItemDetail;
