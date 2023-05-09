c9192407b6402305ab068bda9cc6cf54294d6d59
Alejandro Di Stefano <addistefano@live.com.ar>
2023-05-09 17:49:53 -0300

    Material UI

--- src/components/js/ItemAdmin.js
+++ src/components/js/ItemAdmin.js
@@ -1,12 +1,97 @@
-import { db } from '../../Firebase/firebaseConfig';
-import { doc, updateDoc } from 'firebase/firestore';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { makeStyles, Typography, Button } from '@material-ui/core';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../Firebase/firebaseConfig';
+import { updateDoc } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
+import { db } from '../../Firebase/firebaseConfig';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 
-const ItemAdmin = ({ id, name, category, description, image, size, price, stock }) => {
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 960,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+});
+
+const useStyles = makeStyles((theme) => ({
+    cardAdmin: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        padding: theme.spacing(2),
+        backgroundColor: '#fff',
+        width: '380px',
+        margin: '2rem',
+        [theme.breakpoints.down('sm')]: {
+            width: '100%',
+        },
+    },
+    image: {
+        width: '100%',
+        height: '180px',
+        marginBottom: theme.spacing(2),
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        objectFit: 'contain',
+    },
+    datesAdmin: {
+        marginBottom: theme.spacing(2),
+    },    
+    input: {
+        marginBottom: theme.spacing(2),
+        width: '100%',
+        height: '40px',
+        borderRadius: '4px',
+        border: 'solid 0.5px #eee',
+        fontSize: '1rem',
+    },
+    textarea: {
+        marginBottom: theme.spacing(2),
+        textAlign: 'left',
+        width: '100%',
+        verticalAlign: 'top',
+        height: '120px',
+        borderRadius: '4px',
+        border: 'solid 0.5px #eee',
+        fontSize: '1rem',
+    },
+    button: {
+        marginTop: theme.spacing(2),
+    },
+    payButton: {
+        padding: theme.spacing(1, 2),
+        backgroundColor: '#333',
+        color: '#fff',
+        borderRadius: '4px',
+        textDecoration: 'none',
+        transition: 'background-color 0.3s ease',
+        '&:hover': {
+            backgroundColor: '#333',
+        },
+    },
+}));
+
+const ItemAdmin = ({
+    id,
+    name,
+    category,
+    description,
+    image,
+    size,
+    price,
+    stock,
+}) => {
     const [newName, setName] = useState(name);
     const [newCategory, setCategory] = useState(category);
     const [newDescription, setDescription] = useState(description);
@@ -15,6 +100,7 @@ const ItemAdmin = ({ id, name, category, description, image, size, price, stock 
     const [newStock, setStock] = useState(stock);
     const navigate = useNavigate();
     const [newImageFile, setImageFile] = useState(null);
+    const classes = useStyles();
 
     const handleNameChange = (event) => {
         setName(event.target.value);
@@ -42,64 +128,117 @@ const ItemAdmin = ({ id, name, category, description, image, size, price, stock 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        const productRef = doc(db, "products", id);
+        const productRef = doc(db, 'products', id);
         const updatedProduct = {
             name: newName,
             category: newCategory,
             description: newDescription,
             price: newPrice,
             size: newSize,
-            stock: newStock
+            stock: newStock,
         };
 
         try {
             if (newImageFile) {
-                const storageRef = ref(storage, `img/${newImageFile.name}`);
-                const snapshot = await uploadBytes(storageRef, newImageFile);
-                const imageUrl = await getDownloadURL(snapshot.ref);
-                updatedProduct.image = imageUrl;
-            } else {
-                updatedProduct.image = image;
+                const storageRef = ref(storage, `${id}/${newImageFile.name}`);
+                await uploadBytes(storageRef, newImageFile);
+                const downloadURL = await getDownloadURL(storageRef);
+                updatedProduct.image = downloadURL;
             }
 
             await updateDoc(productRef, updatedProduct);
-            Swal.fire('Éxito', 'Datos guardados en Firestore', 'success');
-            navigate('/admin/itemlist');
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Product updated successfully',
+                showConfirmButton: false,
+                timer: 1500,
+            });
+
+            navigate('/admin/products');
         } catch (error) {
-            Swal.fire('Error', error.message, 'error');
+            console.error('Error updating product:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'An error occurred while updating the product',
+            });
         }
     };
 
     return (
-        <div className="Card CardDetail CardAdmin" id='Item-Detail'>
-            <form className="ContactForm" onSubmit={handleSubmit}>
-                <div>
-                    <img src={image} alt={name} />
-                    <div className='DatesAdmin'>
-                        <h3>Imagen:</h3>
-                        <div className="LonneInput"><input type="file" accept="image/*" onChange={handleImageChange} /></div>
-                        <h3>Nombre: </h3>
-                        <div className="LonneInput"><input type='text' value={newName} onChange={handleNameChange} /></div>
-                        <h3>Categoria: </h3>
-                        <div className="LonneInput"><input type='text' value={newCategory} onChange={handleCategoryChange} /></div>
-                        <h3>Descripcion: </h3>
-                        <div className="LonneInput"><textarea value={newDescription} onChange={handleDescriptionChange} /></div>
-                        <h3>Stock: </h3>
-                        <div className="LonneInput"><input type='number' value={newStock} onChange={handleStockChange} /></div>
-                        <h3>Talle:</h3>
-                        <div className="LonneInput"><input type='text' value={newSize} onChange={handleSizeChange} /></div>
-                        <h3>Precio: $ </h3>
-                        <div className="LonneInput"><input type='number' value={newPrice} onChange={handlePriceChange} /></div>
-                    </div>
-                </div>
-
-                <div className='ComprarFinal BotonAdmin'>
-                    <button type='submit'>
-                        Continuar
-                    </button>
-                </div>
+        <ThemeProvider theme={theme}>
+            <form onSubmit={handleSubmit} className={classes.cardAdmin}>
+                <Typography variant="h1" className='Mini'>Modificar Producto</Typography>
+                <img src={image} alt={name} className={classes.image} />
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className={classes.input}
+                />
+                <input
+                    type="text"
+                    value={newName}
+                    onChange={handleNameChange}
+                    className={classes.input}
+                    placeholder="Name"
+                    required
+                />
+                <input
+                    type="text"
+                    value={newCategory}
+                    onChange={handleCategoryChange}
+                    className={classes.input}
+                    placeholder="Category"
+                    required
+                />
+                <textarea
+                    type="text"
+                    value={newDescription}
+                    onChange={handleDescriptionChange}
+                    className={`${classes.textarea}`}
+                    placeholder="Description"
+                    required
+                />
+                <input
+                    type="number"
+                    min="0"
+                    step="0.01"
+                    value={newPrice}
+                    onChange={handlePriceChange}
+                    className={classes.input}
+                    placeholder="Price"
+                    required
+                />
+                <input
+                    type="text"
+                    value={newSize}
+                    onChange={handleSizeChange}
+                    className={classes.input}
+                    placeholder="Size"
+                    required
+                />
+                <input
+                    type="number"
+                    min="0"
+                    step="1"
+                    value={newStock}
+                    onChange={handleStockChange}
+                    className={classes.input}
+                    placeholder="Stock"
+                    required
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                >
+                    Modificar
+                </Button>
             </form>
-        </div>
+        </ThemeProvider>
     );
 };
 
