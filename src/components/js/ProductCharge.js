f27df99a60cf1a032245c9a3352c1391beb8453a
Alejandro Di Stefano <addistefano@live.com.ar>
2023-05-09 18:07:39 -0300

    Material UI

--- src/components/js/ProductCharge.js
+++ src/components/js/ProductCharge.js
@@ -4,14 +4,64 @@ import { useEffect, useState } from "react";
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { makeStyles, Typography, Button } from '@material-ui/core';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 960,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+});
 
+const useStyles = makeStyles((theme) => ({
+    cardAdmin: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        padding: theme.spacing(2),
+        backgroundColor: '#fff',
+        width: '380px',
+        margin: '2rem',
+        [theme.breakpoints.down('sm')]: {
+            width: '100%',
+        },
+    },
+    input: {
+        marginBottom: theme.spacing(2),
+        width: '100%',
+        height: '40px',
+        borderRadius: '4px',
+        border: 'solid 0.5px #eee',
+        fontSize: '1rem',
+    },
+    textarea: {
+        marginBottom: theme.spacing(2),
+        textAlign: 'left',
+        width: '100%',
+        verticalAlign: 'top',
+        height: '120px',
+        borderRadius: '4px',
+        border: 'solid 0.5px #eee',
+        fontSize: '1rem',
+    },
+    button: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 const ProductCharge = ({ greeting }) => {
     const [loading, setLoading] = useState(false);
     const [authenticated, setAuthenticated] = useState(false);
     const navigate = useNavigate();
-
+    const classes = useStyles();
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -24,10 +74,9 @@ const ProductCharge = ({ greeting }) => {
         return unsubscribe;
     }, []);
 
-
     const handleSubmit = async (event) => {
         event.preventDefault();
-    
+
         const imageFile = event.target.image.files[0];
         const name = event.target.name.value;
         const description = event.target.description.value;
@@ -35,13 +84,13 @@ const ProductCharge = ({ greeting }) => {
         const price = event.target.price.value;
         const size = event.target.size.value;
         const stock = event.target.stock.value;
-    
+
         try {
-            setLoading(true);    
-            const storageRef = ref(storage, `img/${imageFile.name}`);    
-            const snapshot = await uploadBytes(storageRef, imageFile);    
+            setLoading(true);
+            const storageRef = ref(storage, `img/${imageFile.name}`);
+            const snapshot = await uploadBytes(storageRef, imageFile);
             const imageUrl = await getDownloadURL(snapshot.ref);
-    
+
             const products = {
                 name,
                 description,
@@ -51,7 +100,7 @@ const ProductCharge = ({ greeting }) => {
                 image: imageUrl,
                 stock,
             };
-    
+
             const productsCollection = collection(db, "products");
             await addDoc(productsCollection, products);
             Swal.fire('Éxito', 'Datos guardados en Firestore', 'success');
@@ -61,8 +110,8 @@ const ProductCharge = ({ greeting }) => {
             setLoading(false);
         }
     };
-    
 
+    
     const handleOnClick = () => {
         navigate('/');
     };
@@ -75,88 +124,80 @@ const ProductCharge = ({ greeting }) => {
             </div>
         );
     }
+
     return (
-
-        (authenticated) ? (
-            <>
-                <div className="Contacto">
-                    <h1 className='Mini'>{greeting}</h1>
+        <ThemeProvider theme={theme}>
+            {authenticated ? (
+                <div className={classes.cardAdmin}>
+                    <Typography variant="h1" className='Mini'>{greeting}</Typography>
                     <form className="ContactForm" onSubmit={handleSubmit}>
-                        <div className="LonneInput">
-                            <label htmlFor="name">Nombre:</label>
-                            <input
-                                type="text"
-                                name="name"
-                                required
-                            />
-                        </div>
-                        <div className="LonneInput">
-                            <label htmlFor="username">Categoría:</label>
-                            <input
-                                type="text"
-                                name="category"
-                                required
-                            />
-                        </div>
-                        <div className="LonneInput">
-                            <label htmlFor="description">Descripción:</label>
-                            <textarea
-                                name="description"
-                                required
-                            />
-                        </div>
-                        <div className="LonneInput">
-                            <label htmlFor="password">Precio:</label>
-                            <input
-                                type="number"
-                                name="price"
-                                required
-                            />
-                        </div>
-                        <div className="LonneInput">
-                            <label htmlFor="image">Imagen:</label>
-                            <input
-                                type="file"
-                                name="image"
-                                accept="image/*"
-                                required
-                            />
-                        </div>
-                        <div className="LonneInput">
-                            <label htmlFor="password">Talle:</label>
-                            <input
-                                type="text"
-                                name="size"
-                                required
-                            />
-                        </div>
-                        <div className="LonneInput">
-                            <label htmlFor="password">Stock:</label>
-                            <input
-                                type="number"
-                                name="stock"
-                                required
-                            />
-                        </div>
-                        <div className="ComprarFinal FinalButtons">
-                            <button
-                                type="submit">
-                                Cargar Producto
-                            </button>
-                        </div>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Nombre"
+                            required
+                            className={classes.input}
+                        />
+                        <input
+                            type="text"
+                            name="category"
+                            placeholder="Categoría"
+                            required
+                            className={classes.input}
+                        />
+                        <textarea
+                            name="description"
+                            placeholder="Descripción"
+                            required
+                            className={`${classes.textarea}`}
+                        />
+                        <input
+                            type="number"
+                            name="price"
+                            placeholder="Precio"
+                            required
+                            className={classes.input}
+                        />
+                        <input
+                            type="file"
+                            name="image"
+                            accept="image/*"
+                            required
+                            className={classes.input}
+                        />
+                        <input
+                            type="text"
+                            name="size"
+                            placeholder="Talle"
+                            required
+                            className={classes.input}
+                        />
+                        <input
+                            type="number"
+                            name="stock"
+                            placeholder="Stock"
+                            required
+                            className={classes.input}
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                        >
+                            Cargar Producto
+                        </Button>
                     </form>
-
                 </div>
-            </>) : (
-            <>
-                <div className="ButtonItemListDetail">
-                    <button onClick={handleOnClick}>Volver</button>
-                </div>
-                <h3>No esta Autorizado para acceder a esta Página</h3>
-            </>
-        )
-
-
+            ) : (
+                <>
+                    <div className="ButtonItemListDetail">
+                        <button onClick={handleOnClick}>Volver</button>
+                    </div>
+                    <h3>No está autorizado para acceder a esta página</h3>
+                </>
+            )}
+        </ThemeProvider>
     );
 };
 
